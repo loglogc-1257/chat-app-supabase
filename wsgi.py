938ce2d092848cd5b1eaa469dce37257bea98e35
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-07-02 07:46:09 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -15,16 +15,10 @@ os.makedirs(VOICE_FOLDER, exist_ok=True)
 import init_and_migrate
 init_and_migrate.init_db_schema()
 
-# Point d'entrée pour les serveurs WSGI
-application = socketio
-"""
-Point d'entrée WSGI pour le déploiement en production
-"""
-
-# Point d'entrée pour les serveurs WSGI (Render, Heroku, etc.)
-application = socketio
+# Point d'entrée WSGI pour Render
+application = app
 
 if __name__ == "__main__":
     # Pour le développement local
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
