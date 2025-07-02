74f110d14aa934f90dc31c7919d650e51dd9e38b
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-07-02 07:47:50 +0100

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,24 +1,17 @@
 
 import os
 
-# Configuration Gunicorn optimisée pour le déploiement
+# Configuration Gunicorn simplifiée pour Render
 bind = f"0.0.0.0:{os.environ.get('PORT', 5000)}"
 workers = 1
-worker_class = "gevent"
-worker_connections = 1000
+worker_class = "sync"
 timeout = 120
-keepalive = 5
+keepalive = 2
 max_requests = 1000
-max_requests_jitter = 100
+max_requests_jitter = 50
 preload_app = True
 
 # Logging
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-
-# Security
-limit_request_line = 4094
-limit_request_fields = 100
-limit_request_field_size = 8190
